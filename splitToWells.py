b9864192ed479aac04520f1edf6bd485fe0fe34a
Nadav Angel <nangel@parallelwireless.com>
2023-09-08 16:48:28 +0300

    Add dest

--- splitToWells.py
+++ splitToWells.py
@@ -13,6 +13,7 @@ def main() -> int:  # pragma: no cover
 	
 	parser = argparse.ArgumentParser(prog="SplitToWells")
 	parser.add_argument("-f", "--folder", type=pathlib.Path)
+	parser.add_argument("-d", "--dest", type=pathlib.Path)
 	parser.add_argument('-v', '--verbose', help='debug logs', action='store_const', const=logging.DEBUG,
 	                    default=logging.INFO)
 	args = parser.parse_args()
@@ -20,16 +21,26 @@ def main() -> int:  # pragma: no cover
 	                    format='%(asctime)s %(levelname)-7s %(message)s')
 	
 	if args.folder is None:
-		folder = filedialog.askdirectory()
+		folder = filedialog.askdirectory(title="Select source flder")
 	else:
 		folder = args.folder
-	
+		
 	if folder is None or folder == "":
-		logging.info("No folder was selected")
+		logging.info("No source folder was selected")
 		return 0
+		
+	if args.dest is None:
+		dest = filedialog.askdirectory(title="Select destention flder")
+	else:
+		dest = args.folder
+	
+	if dest is None or dest == "":
+		logging.info("No destention folder was selected")
+		return 0
+	
 	start = time.time()
 	mic = photo.Microscope(folder=folder)
-	dest = mic.move(folder)
+	dest = mic.move(dest)
 	end = time.time()
 	total_time = datetime.timedelta(seconds=end-start)
 	
